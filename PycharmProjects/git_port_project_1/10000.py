4af730cf174ea95184462663f1151f7fe2df173b
Mitch Tucker <mrsnmrtucker@gmail.com>
2025-06-19 20:32:53 -0600

    added comment inside header function explaining mis-alignment

--- PycharmProjects/git_port_project_1/10000.py
+++ PycharmProjects/git_port_project_1/10000.py
@@ -2,6 +2,8 @@
 # rolls are random, and player can select die or dice to keep/ re-roll
 
 def show_title():
+    #display a title/header
+    #i noticed this lines up perfectly in my code editor but mis-aligned when viewed on github
     print("""
     ---------------------------------------------------------------------------------------------------------------------------------------------------
     |           O                         OOO                             OOO                              OOO                             OOO           |
