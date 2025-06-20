89f5e9ac320740b071b0f94ce074fde81f2e2ba1
Mitch Tucker <mrsnmrtucker@gmail.com>
2025-06-19 20:28:27 -0600

    added a basic function to display a header at the start

--- PycharmProjects/git_port_project_1/10000.py
+++ PycharmProjects/git_port_project_1/10000.py
@@ -1,2 +1,17 @@
 # Text based 10,000 dice game, where num of npc opponents can be chosen,
 # rolls are random, and player can select die or dice to keep/ re-roll
+
+def show_title():
+    print("""
+    ---------------------------------------------------------------------------------------------------------------------------------------------------
+    |           O                         OOO                             OOO                              OOO                             OOO           |
+    |        OO                      O         O                       O         O                        O         O                       O         O        |
+    |     O   O                   O               O                 O               O                  O               O                 O               O     |
+    |           O                   O               O                 O               O                  O               O                 O               O     |
+    |           O                   O               O                 O               O                  O               O                 O               O     |
+    |           O                      O         O          /            O         O                        O         O                       O         O        |
+    |     OOOOO                   OOO           /                 OOO                              OOO                             OOO           |
+    ---------------------------------------------------------------------------------------------------------------------------------------------------
+    """)
+
+show_title()
